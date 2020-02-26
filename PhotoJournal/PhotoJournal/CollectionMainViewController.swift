b9483ab58fc98bb11bab85b12557ea3d3480c49c
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-26 14:29:47 -0500

    working on constraints

--- PhotoJournal/PhotoJournal/CollectionMainViewController.swift
+++ PhotoJournal/PhotoJournal/CollectionMainViewController.swift
@@ -44,14 +44,14 @@ extension CollectionMainViewController: UICollectionViewDataSource {
 // here we are using UICollectionViewFlowLayout
 extension CollectionMainViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let maxSize: CGSize = UIScreen.main.bounds.size
-        let itemWidth: CGFloat = maxSize.width
-        let itemHeight: CGFloat = maxSize.height * 0.50
-        return CGSize(width: itemWidth, height: itemHeight)
+        //FIXME: cannot set "normal" constraints
+        let maxWidth: CGFloat = UIScreen.main.bounds.size.width
+        let itemWidth: CGFloat = maxWidth * 0.80
+        return CGSize(width: itemWidth, height: itemWidth)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+        return UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
