ef4faecd834f9f0f6ed295c3297b80c2150212b2
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-26 13:57:38 -0500

    setting size for collection view cells

--- PhotoJournal/PhotoJournal/CollectionMainViewController.swift
+++ PhotoJournal/PhotoJournal/CollectionMainViewController.swift
@@ -16,10 +16,47 @@ class CollectionMainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //FIXME (give the user option to change the color)
+        view.backgroundColor = .systemOrange
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension CollectionMainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //FIXME: return count fo photos
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionPhotoCell", for: indexPath) as? CollectionPhotoCell else {
+            fatalError("could downcast to CollectionPhotoCell")
+        }
+        //let dogImage = dogImages[indexPath.row]
+        //cell.configureCell(with: dogImage)
+        
+        cell.backgroundColor = .yellow
+        //cell.delegate = self
+        return cell
+    }
+}
+
+// here we are using UICollectionViewFlowLayout
+extension CollectionMainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.50
+        return CGSize(width: itemWidth, height: itemHeight)
     }
     
-
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
 }
 
