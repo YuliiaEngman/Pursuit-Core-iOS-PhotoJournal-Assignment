57eb0b8410624542489c7685aafc5019ea7fa511
Yuliia Engman <yuliia.engman@gmail.com>
2020-04-30 18:12:52 -0400

    organized files and set code to photoCell

---
+++ PhotoJournal/PhotoJournal/Cell/CollectionPhotoCell.swift
@@ -0,0 +1,58 @@
+//
+//  CollectionPhotoCell.swift
+//  PhotoJournal
+//
+//  Created by Yuliia Engman on 2/26/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+protocol CollectionPhotoCellDelegate: AnyObject {
+    func didLongPress(_ collectionPhotoCell: CollectionPhotoCell)
+}
+
+class CollectionPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    @IBOutlet weak var photoNameLabel: UILabel!
+    
+    weak var delegate: CollectionPhotoCellDelegate?
+    
+    private lazy var longPressGesture: UILongPressGestureRecognizer = {
+          let gesture = UILongPressGestureRecognizer()
+          gesture.addTarget(self, action: #selector(longPressedAction(gesture:)))
+          return gesture
+      }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 20.0
+        backgroundColor = .orange
+        
+        addGestureRecognizer(longPressGesture)
+    }
+    
+    @objc
+       private func longPressedAction(gesture: UILongPressGestureRecognizer) {
+           if gesture.state == .began {// if gesture is active
+               gesture.state = .cancelled
+               return
+           }
+           delegate?.didLongPress(self)
+       }
+    
+    public func configureCell(imageObject: ImageObject) {
+        // convertion Data to UIImage
+        guard let image = UIImage(data: imageObject.imageData) else {
+            return
+        }
+        photoImage.image = image
+        photoNameLabel.text = ""
+    }
+    
+  
+    
+    
+}
---
+++ PhotoJournal/PhotoJournal/Model/ImageObject.swift
@@ -0,0 +1,15 @@
+//
+//  ImageObject.swift
+//  PhotoJournal
+//
+//  Created by Yuliia Engman on 4/30/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import Foundation
+
+struct ImageObject: Codable {
+  let imageData: Data // we cannot use UIImage directly, we have to work through Data
+  let date: Date
+  let identifier = UUID().uuidString // this will help us to create a unique identifier, creates authomatically
+}
