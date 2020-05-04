d1e8457eb4327676bd909922fca2e0ffe5bb9d17
Yuliia Engman <yuliia.engman@gmail.com>
2020-05-04 12:00:50 -0400

    updated UI for the add VC

--- PhotoJournal/PhotoJournal/NewControllers/NewAddPhotoViewController.swift
+++ PhotoJournal/PhotoJournal/NewControllers/NewAddPhotoViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class NewAddPhotoViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonPressed(_ sender: UIButton) {
     }
-    */
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func photolibraryButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraButton(_ sender: UIButton) {
+    }
+    
 
 }
